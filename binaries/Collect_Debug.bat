9f4abad82677afe68a915441f3f91a9ec6a47d73
George Papadopoulos <gepa@nanoradio.com>
2012-12-06 11:07:06 +0200

    Adding files for from 4.3.8b (http://en.poderosa.org/download/source.html)
    http://en.poderosa.org/

---
+++ binaries/Collect_Debug.bat
@@ -0,0 +1,33 @@
+copy /Y ..\bin\Debug\Poderosa.exe
+
+copy /Y ..\bin\Debug\Granados.dll
+
+copy /Y ..\bin\Debug\Poderosa.Plugin.dll
+
+copy /Y ..\bin\Debug\Core\Poderosa.Core.dll Core\
+
+copy /Y ..\bin\Debug\Macro\Poderosa.Macro.dll Macro\
+
+copy /Y ..\bin\Debug\Pipe\Poderosa.Pipe.dll Pipe\
+
+copy /Y ..\bin\Debug\Portforwarding\Poderosa.Portforwarding.dll Portforwarding\
+
+copy /Y ..\bin\Debug\PortForwardingCommand\Poderosa.PortForwardingCommand.dll PortForwardingCommand\
+
+copy /Y ..\bin\Debug\Protocols\Poderosa.Protocols.dll Protocols\
+
+copy /Y ..\bin\Debug\SerialPort\Poderosa.SerialPort.dll SerialPort\
+
+copy /Y ..\bin\Debug\SFTP\Poderosa.SFTP.dll SFTP\
+
+copy /Y ..\bin\Debug\TerminalEmulator\Poderosa.TerminalEmulator.dll TerminalEmulator\
+
+copy /Y ..\bin\Debug\TerminalSession\Poderosa.TerminalSession.dll TerminalSession\
+
+copy /Y ..\bin\Debug\UI\Poderosa.UI.dll UI\
+
+copy /Y ..\bin\Debug\Usability\Poderosa.Usability.dll Usability\
+
+copy /Y ..\bin\Debug\XZModem\Poderosa.XZModem.dll XZModem\
+
+
---
+++ binaries/Collect_Release.bat
@@ -0,0 +1,33 @@
+copy /Y ..\bin\Release\Poderosa.exe
+
+copy /Y ..\bin\Release\Granados.dll
+
+copy /Y ..\bin\Release\Poderosa.Plugin.dll
+
+copy /Y ..\bin\Release\Core\Poderosa.Core.dll Core\
+
+copy /Y ..\bin\Release\Macro\Poderosa.Macro.dll Macro\
+
+copy /Y ..\bin\Release\Pipe\Poderosa.Pipe.dll Pipe\
+
+copy /Y ..\bin\Release\Portforwarding\Poderosa.Portforwarding.dll Portforwarding\
+
+copy /Y ..\bin\Release\PortForwardingCommand\Poderosa.PortForwardingCommand.dll PortForwardingCommand\
+
+copy /Y ..\bin\Release\Protocols\Poderosa.Protocols.dll Protocols\
+
+copy /Y ..\bin\Release\SerialPort\Poderosa.SerialPort.dll SerialPort\
+
+copy /Y ..\bin\Release\SFTP\Poderosa.SFTP.dll SFTP\
+
+copy /Y ..\bin\Release\TerminalEmulator\Poderosa.TerminalEmulator.dll TerminalEmulator\
+
+copy /Y ..\bin\Release\TerminalSession\Poderosa.TerminalSession.dll TerminalSession\
+
+copy /Y ..\bin\Release\UI\Poderosa.UI.dll UI\
+
+copy /Y ..\bin\Release\Usability\Poderosa.Usability.dll Usability\
+
+copy /Y ..\bin\Release\XZModem\Poderosa.XZModem.dll XZModem\
+
+
